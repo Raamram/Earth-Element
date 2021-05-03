e695b91fe5ba9289a410fc04ae5cf9118af54de5
Raamram <rexrkram@outlook.com>
2021-05-03 21:30:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Expense from "./components/Expense/Expense";
+
+const App = () =>{
+  return (
+    <div>
+      <h1>Hi React</h1>
+      <Expense />
+    </div>
+  );
+}
+
+export default App;
